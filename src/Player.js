54e33eaaa63876a00a588d942f968fc6aefec6e0
crux <>
2024-08-02 18:08:04 -0400

    for gitpages

--- src/Player.js
+++ src/Player.js
@@ -14,7 +14,7 @@ export default function Player({
   // Automatically load the music when the component mounts
   useEffect(() => {
     const music = new Howl({
-      src: ['/mixtape.ogg'], // Update with your actual audio file path
+      src: ['to-stace/mixtape.ogg'], // Update with your actual audio file path
       html5: true,
       onload: () => {
         setSelectedMusic(music);
